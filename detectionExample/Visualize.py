bad45a31eae2d9a121132d0424abf3da25822745
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2018-10-12 16:26:46 -0700

    Add files via upload

--- detectionExample/Visualize.py
+++ detectionExample/Visualize.py
@@ -38,8 +38,9 @@ def Visualize(img, results):
 
 
 
-def VisualizeBox(img, results, outputdir):
+def VisualizeCamera(img, results, outputdir):
     img_cp = img.copy()
+    #print "type = ", type(img_cp)
     crop_imgs = []
     count = 0
     detectedNum = len(results)
@@ -59,6 +60,8 @@ def VisualizeBox(img, results, outputdir):
                 confidence = format(confidence,'.2f') 
                 txt = txt+' '+str(confidence)+'%'
 
+                #print left, " ", top , " ", right , " ", bottom
+ 
                 h = bottom - top
                 w = right - left
                 x1 = left - 5
@@ -67,15 +70,75 @@ def VisualizeBox(img, results, outputdir):
                 y2 = top+w+5
                 crop_img = img_cp[y1:y2, x1:x2]
 
+                #print img_cp.size
+                #print crop_img.size
+                if crop_img.size > 150528:
+                    cv2.rectangle(img_cp, (x1,y1), (x2,y2), clr, thickness=1)
+                    size = cv2.getTextSize(txt, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 1)
+                    width = size[0][0]
+                    height = size[0][1]
 
+                    cv2.rectangle(img_cp, (x1,(y2-5) - (height + 2)),((x1 + width),(y2-5)),clr,-1)
+                    cv2.putText(img_cp,txt,(x1,(y2-5)),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.5,(20,20,20),1)
+                    """
+                    path = os.path.join(outputdir ,  'yolo-output_'+ str(count) + '.jpg')
+                    #cv2.imshow('AMD YoloV2 Live', crop_img)
+                    cv2.imwrite(path,crop_img)
+                    count += 1
+                    """
+                    crop_imgs.append(crop_img)
+    
+    return crop_imgs
+
+def VisualizeBox(image, img, results, outputdir, anniedir):
+    img_cp = img.copy()
+    #print "type img_cp= ", type(img_cp)
+    crop_imgs = []
+    count = 0
+    detectedNum = len(results)
+  
+    if detectedNum > 0:
+            for i in range(detectedNum):
                 
-                cv2.rectangle(img_cp, (x1,y1), (x2,y2), clr, thickness=1)
+                clr = colors[results[i].objType % colornum]
+                txt = results[i].name
+
+                left = results[i].left
+                top = results[i].top
+                right = results[i].right
+                bottom = results[i].bottom
+                confidence = results[i].confidence
+                confidence = confidence * 100
+                confidence = format(confidence,'.2f') 
+                txt = txt+' '+str(confidence)+'%'
+
+                orig_image = cv2.imread(anniedir + image)
+                        #print left, " ", top , " ", right , " ", bottom
+                #print "type orig_image= ", type(orig_image)    
+                x1 = int((left/416) * 1024)
+                x2 = int((right/416) * 1024)
+                y1 = int((top/416) * 1024)
+                y2 = int((bottom/416) * 1024)
+                """
+                h = bottom - top
+                w = right - left
+                x1 = left - 5
+                x2 = left + h+ 5
+                y1 = top -5
+                y2 = top+w+5
+                """
+                crop_img = orig_image[y1:y2, x1:x2]
+
+                #print img_cp.size
+                #print crop_img.size
+                
+                cv2.rectangle(orig_image, (x1,y1), (x2,y2), clr, thickness=3)
                 size = cv2.getTextSize(txt, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 1)
                 width = size[0][0]
                 height = size[0][1]
 
-                cv2.rectangle(img_cp, (x1,(y2-5) - (height + 2)),((x1 + width),(y2-5)),clr,-1)
-                cv2.putText(img_cp,txt,(x1,(y2-5)),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.5,(20,20,20),1)
+                cv2.rectangle(orig_image, (x1,(y2-5) - (height + 2)),((x1 + width),(y2-5)),clr,-1)
+                cv2.putText(orig_image,txt,(x1,(y2-5)),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.5,(20,20,20),1)
                 """
                 path = os.path.join(outputdir ,  'yolo-output_'+ str(count) + '.jpg')
                 #cv2.imshow('AMD YoloV2 Live', crop_img)
@@ -85,4 +148,3 @@ def VisualizeBox(img, results, outputdir):
                 crop_imgs.append(crop_img)
     
     return crop_imgs
-            
